c531751c885c6afe49cd90a4ef32f8bf52ee46d1
olvarfo <161972348+olvarfo@users.noreply.github.com>
2024-04-26 19:54:52 +0300

    Add files via upload

--- платформер/blocks.py
+++ платформер/blocks.py
@@ -1,5 +1,9 @@
 from pygame import *
 
+'''''
+мы импортируем библиотеку пайгейм 
+'''''
+
 PLATFORM_WIDTH =32
 PLATFORM_HEIGHT=32
 PLATFORM_COLOR= "#FF6262"
@@ -11,3 +15,8 @@ class  Platform (sprite.Sprite):
         self.image.fill(Color(PLATFORM_COLOR))
         self.rect=Rect(x,y,PLATFORM_WIDTH,PLATFORM_HEIGHT)
 
+        '''''
+        мы создаем функцию self которая self — это ссылка на текущий 
+        экземпляр класса. Это способ обращения к атрибутам и методам класса
+         изнутри самого класса
+        '''''
--- платформер/coins.py
+++ платформер/coins.py
@@ -1,19 +1,33 @@
 from pygame import *
+'''''
+мы импортируем библеотеку пайгейм
+'''''
 import pygame
 
 
 class Coins (sprite.Sprite):
+   """
+   мы создаем класс —  это шаблон, с помощью которого удобно описывать
+   однотипные объекты. В классе соержатся свойства, правила создания
+    и поведение объекта. Объект — экземпляр, созданный на основе шаблона.
+     Атрибут — поле, хранящее значение.Мы создаем сласс коинс для того чтобы
+     сделать в будующим монетки
 
-     def __init__(self,x,y):
+     """
 
-        sprite.Sprite.__init__(self)
 
-        self.image = pygame.image.load ("images/coin.png")
 
-        self.rect = self.image.get_rect()
 
-        self.rect.x=x
-        self.rect.y=y
+   def __init__(self,x,y):
+
+     sprite.Sprite.__init__(self)
+
+     self.image = pygame.image.load ("images/coin.png")
+
+     self.rect = self.image.get_rect()
+
+     self.rect.x=x
+     self.rect.y=y
 
 
 
