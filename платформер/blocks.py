6811cba3a65cb4df4d9b45bc0949844765c6e0fb
olvarfo <161972348+olvarfo@users.noreply.github.com>
2024-04-28 20:57:36 +0300

    Update blocks.py

--- платформер/blocks.py
+++ платформер/blocks.py
@@ -7,12 +7,20 @@ from pygame import *
 PLATFORM_WIDTH =32
 PLATFORM_HEIGHT=32
 PLATFORM_COLOR= "#FF6262"
-с
+
 class Platform (sprite.Sprite):
     """
-    мы создаем класс Platform 
+    мы создаем класс Platform .класс —  это шаблон, с помощью которого удобно описывать
+   однотипные объекты. В классе соержатся свойства, правила создания
+    и поведение объекта. Объект — экземпляр, созданный на основе шаблона.
+     Атрибут — поле, хранящее значение.мы создаём класс платформер для того чтобы сделать твёрдые 
+     платформы
     """
     def __init__ (self,x,y):
+        """
+        мы создаём функцию init
+        """
+        
         sprite.Sprite.__init__(self)
         self.image=Surface((PLATFORM_WIDTH,PLATFORM_HEIGHT))
         self.image.fill(Color(PLATFORM_COLOR))
